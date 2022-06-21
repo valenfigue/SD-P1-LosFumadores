3b8661e9a803fa15789f1dafd501afe22f6fb1f0
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2022-06-20 20:03:21 -0400

    WIP: Vendor
    Creating classes and tests. Most of the methods still empty.

---
+++ src/test/java/com/mycompany/app/VendorTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import junit.framework.TestCase;
+
+public class VendorTest extends TestCase {
+	
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testTestRun() {
+	}
+	
+	public void testReplenishIngredientsOnBenches() {
+	}
+}
