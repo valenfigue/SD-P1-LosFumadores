6e34c116293ef528085cd3ec3b746eda26636583
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2022-06-23 15:35:51 -0400

    WIP: refactoring
    The smoker's main program can be reuse.

--- src/main/java/com/mycompany/app/clients/RunSmokerWithTobacco.java
+++ src/main/java/com/mycompany/app/clients/RunSmokerWithTobacco.java
@@ -3,48 +3,12 @@ package com.mycompany.app.clients;
 import com.mycompany.app.smokers.SmokerWithTobacco;
 
 import java.io.*;
-import java.net.Socket;
 
 public class RunSmokerWithTobacco {
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		RunnerClient runnerClient = new RunnerClient();
-		int exit = 2; // The user doesn't want to end the program.
-		
+	public static void main(String[] args) throws IOException {
+		RunnerSmoker runnerClient = new RunnerSmoker();
 		SmokerWithTobacco smoker = new SmokerWithTobacco();
 		
-		do {
-			if (!smoker.checkCigarIngredients()) {
-				if (smoker.getTriesCount() < 3) {
-					try (Socket socket = runnerClient.selectBench()) {
-						ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-						objectOutputStream.writeObject(smoker);
-						
-						smoker.setSocket(socket);
-						smoker.sendCigar();
-						smoker.receiveCigar();
-						smoker.increaseTriesCount();
-					}
-				} else {
-					try (Socket socket = new Socket("localhost", 4999)) {
-						DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-						dataOutputStream.writeUTF("Ingredients needed.");
-						
-						System.out.println("El " + smoker.getActorName().toLowerCase() + " está esperando por el vendedor.");
-						
-						/*DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-						String vendorRespond = dataInputStream.readUTF();
-						
-						if (vendorRespond.equals("Vendor ended their job.")) {
-							dataOutputStream.writeUTF("OK");
-							socket.close();
-						}*/
-						smoker.restartTriesCount();
-					}
-				}
-			} else {
-				smoker.rollCigar();
-				exit = runnerClient.askUserToEndProgram();
-			}
-		} while (exit == 2);
+		runnerClient.mainProgram(smoker);
 	}
 }
--- src/main/java/com/mycompany/app/clients/RunnerClient.java
+++ src/main/java/com/mycompany/app/clients/RunnerClient.java
@@ -6,23 +6,8 @@ import java.util.Scanner;
 
 public class RunnerClient {
 	
-	public Socket selectBench() throws IOException {
-		Scanner sn = new Scanner(System.in);
-		sn.useDelimiter("\n");
-		
-		System.out.println("¿De cuál banca quisiera tomar los ingredientes faltantes para su cigarro?");
-		System.out.println("Banca 1: tiene tabacos.");
-		System.out.println("Banca 2: tiene fósforos.");
-		System.out.println("Banca 3: tiene papeles.");
-		System.out.print("Respuesta: ");
-		int benchNumber = sn.nextInt();
-		
-		return this.getBenchSocket(benchNumber);
-	}
-	
 	public Socket getBenchSocket(int benchNumber) throws IOException {
-		// TODO: arreglar firewall para aceptar peticiones externas.
-		String externalIP = "192.168.1.5"; // IP según geolocalizador. TODO verificar.
+		String externalIP = "192.168.1.5";
 		String localIP = "localhost";
 		
 		String ip = "";
@@ -45,31 +30,5 @@ public class RunnerClient {
 		}
 		
 		return new Socket(ip, port);
-	}
-	
-	public int askUserToEndProgram() {
-		Scanner sn = new Scanner(System.in);
-		sn.useDelimiter("\n");
-		int userAnswer = 0;
-		
-		do {
-			System.out.println("¿Desea terminar?");
-			System.out.println("1.- Sí.");
-			System.out.println("2.- No.");
-			System.out.print("Respuesta: ");
-			try {
-				userAnswer = sn.nextInt();
-			} catch (Exception e) {
-				System.out.println("No existe esa opción.");
-			}
-			
-			if (userAnswer != 1 && userAnswer != 2) {
-				System.out.println("No existe esa opción.");
-			}
-			
-		} while (userAnswer != 1 && userAnswer != 2);
-		
-		return userAnswer;
-		
 	}
 }
