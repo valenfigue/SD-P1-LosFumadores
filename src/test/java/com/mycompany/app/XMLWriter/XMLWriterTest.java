0bff598803ce8e4bb5bc9c3631d546834cd07e79
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2022-06-24 12:17:22 -0400

    WIP: motion-trace file creation
    Testing if the XMLWriter can save more than one activity before to update the XML file.

--- src/test/java/com/mycompany/app/XMLWriter/XMLWriterTest.java
+++ src/test/java/com/mycompany/app/XMLWriter/XMLWriterTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.app.XMLWriter;
 
 import com.mycompany.app.Client;
+import com.mycompany.app.smokers.SmokerWithMatchstick;
 import com.mycompany.app.smokers.SmokerWithPaper;
 import junit.framework.TestCase;
 
@@ -21,6 +22,27 @@ public class XMLWriterTest extends TestCase {
 		
 		try {
 			writer.updateMotionTrace(smoker, action, quantity);
+			writer.updateFile();
+		} catch (TransformerException e) {
+			fail("Hubo un problema al escribir el archivo de trazas de movimientos para este actor.");
+		}
+	}
+	
+	public void testUpdateFileWithTwoActivities() {
+		Client smoker1 = new SmokerWithPaper();
+		String action1 = smoker1.getActorName() + " está probando la creación de un XML";
+		int quantity1 = 1;
+		
+		Client smoker2 = new SmokerWithMatchstick();
+		String action2 = smoker2.getActorName() + " también está probando la creación de un XML";
+		int quantity2 = 2;
+		
+		try {
+			// First activity
+			writer.updateMotionTrace(smoker1, action1, quantity1);
+			// Second activity
+			writer.updateMotionTrace(smoker2, action2, quantity2);
+			
 			writer.updateFile();
 		} catch (TransformerException e) {
 			fail("Hubo un problema al escribir el archivo de trazas de movimientos para este actor.");
